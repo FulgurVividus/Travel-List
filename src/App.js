db9eb31d98c8878ea8e371984868a680502e16d8
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-06-09 12:40:33 +0500

    feat: created 4 components

--- src/App.js
+++ src/App.js
@@ -1,1 +1,50 @@
-export default function App() {}
+export default function App() {
+  return (
+    <>
+      <div className="app">
+        <Logo />
+        <Form />
+        <PackingList />
+        <Stats />
+      </div>
+    </>
+  );
+}
+
+// Logo component
+function Logo() {
+  return (
+    <>
+      <h1>🏝️ Far Away 🧳</h1>
+    </>
+  );
+}
+
+// Form component
+function Form() {
+  return (
+    <>
+      <div className="add-form">What do you need for your 😍 trip?</div>
+    </>
+  );
+}
+
+// PackingList component
+function PackingList() {
+  return (
+    <>
+      <div className="list">LIST</div>
+    </>
+  );
+}
+
+// Stats component
+function Stats() {
+  return (
+    <>
+      <footer className="stats">
+        <em>💼 You have X items on your list, and you already packed X (X%)</em>
+      </footer>
+    </>
+  );
+}
