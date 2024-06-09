1c852fde4445f181dc3e2a3245a3fe9106cfbe8a
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-06-09 14:29:04 +0500

    feat: applied controlled elements in the Form component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -27,24 +29,51 @@ function Logo() {
 }
 
 // Form component
+// in order to implement the "controlled elements" technique, we follow 3 steps
 function Form() {
+  // 1. create a piece of "state"
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   const handleSubmit = function (e) {
     e.preventDefault();
-    console.log(e);
+
+    // guard close
+    if (!description) {
+      return;
+    }
+
+    // object of new item
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    // cleaning OR setting the "states" back to their initial states
+    setDescription("");
+    setQuantity(1);
   };
 
   return (
     <>
       <form className="add-form" onSubmit={handleSubmit}>
         <h3>What do you need for your 😍 trip?</h3>
-        <select>
+        <select
+          value={quantity}
+          onChange={(e) => setQuantity(Number(e.target.value))}
+        >
           {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
             <option value={num} key={num}>
               {num}
             </option>
           ))}
         </select>
-        <input type="text" placeholder="Item..." />
+        {/* 2. use the "state" as the "value" of input field, use this "state" on the element that we wanna control */}
+        <input
+          type="text"
+          placeholder="Item..."
+          value={description}
+          // 3. we need to somehow connect the "state" with the "value", to update that "state variable"
+          onChange={(e) => setDescription(e.target.value)}
+        />
         <button>Add</button>
       </form>
     </>
@@ -90,3 +119,9 @@ function Stats() {
     </>
   );
 }
+
+//! SOME NOTES
+// In order to implement the "controlled elements" technique, we follow 3 steps:
+// 1. create a piece of "state"
+// 2. use this "state" on the element that we wanna control
+// 3. we need to somehow connect the "state" with the "value", to update that "state variable" we do it with onChange={} handler
