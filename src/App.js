0793c6eedd08ecbba482000ebfa4bf357955afb4
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-06-10 18:54:35 +0500

    feat: lifted the items state up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "Charger", quantity: 1, packed: false },
-];
-
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  const handleAddItems = function (item) {
+    // [ current items + new item ]
+    setItems((items) => [...items, item]);
+  };
+
   return (
     <>
       <div className="app">
         <Logo />
-        <Form />
-        <PackingList />
+        <Form onAddItems={handleAddItems} />
+        <PackingList items={items} />
         <Stats />
       </div>
     </>
@@ -30,7 +31,7 @@ function Logo() {
 
 // Form component
 // in order to implement the "controlled elements" technique, we follow 3 steps
-function Form() {
+function Form({ onAddItems }) {
   // 1. create a piece of "state"
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
@@ -46,6 +47,8 @@ function Form() {
     // object of new item
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
+
+    onAddItems(newItem);
 
     // cleaning OR setting the "states" back to their initial states
     setDescription("");
@@ -81,12 +84,12 @@ function Form() {
 }
 
 // PackingList component
-function PackingList() {
+function PackingList({ items }) {
   return (
     <>
       <div className="list">
         <ul>
-          {initialItems.map((item) => (
+          {items.map((item) => (
             <Item item={item} key={item.id} />
           ))}
         </ul>
